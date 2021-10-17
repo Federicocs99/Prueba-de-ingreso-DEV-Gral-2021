b50b4cdccada7a3212f14e239826d27b1c6a5f41
Federicocs99 <fcardonas@unal.edu.co>
2021-10-17 10:34:00 -0500

    :100:

--- Problema2/Controllers/HomeController.cs
+++ Problema2/Controllers/HomeController.cs
@@ -11,27 +11,17 @@ namespace Problema2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            var vuelo = new Flight();
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index(Flight vuelo)
         {
+            Console.WriteLine(vuelo.DepartureDate);
+
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Problema2/Models/Flight.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Problema2.Models
+{
+    public class Flight
+    {
+        public string DepartureStation { get; set; }
+        public string ArrivalStation { get; set; }
+        public DateTime DepartureDate { get; set; }
+
+    }
+}
+
